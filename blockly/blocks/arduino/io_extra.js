2cca7142f02e81db7eaa2aef0047d48c34637d75
Benny Malengier <benny.malengier@gmail.com>
2016-03-21 09:45:48 +0100

    changes present in Blockly4Arduino

---
+++ blockly/blocks/arduino/io_extra.js
@@ -0,0 +1,189 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Blocks for Arduino Digital and Analogue input and output
+ *     functions. The Arduino function syntax can be found at
+ *     http://arduino.cc/en/Reference/HomePage
+ *
+ * TODO: maybe change this to a "PIN" BlocklyType
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.io_extra');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly.Types');
+goog.require('Blockly.io');
+
+Blockly.Blocks['io_pin_dig'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_PIN_DIG)
+        .appendField(new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins), "PIN");
+    this.setOutput(true);
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.setTooltip(Blockly.Msg.ARD_PIN_DIG_TIP);
+    this.setHelpUrl('https://www.arduino.cc/en/Reference/Board');
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.NUMBER;
+  },
+  /**
+   * Updates the content of the the pin related fields.
+   * @this Blockly.Block
+   */
+  updateFields: function() {
+    Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+        this, 'PIN', 'digitalPins');
+  }
+};
+
+Blockly.Blocks['io_pin_an'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_PIN_AN)
+        .appendField(new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.analogPins), "PIN");
+    this.setOutput(true);
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.setTooltip(Blockly.Msg.ARD_PIN_AN_TIP);
+    this.setHelpUrl('https://www.arduino.cc/en/Reference/Board');
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.NUMBER;
+  },
+  /**
+   * Updates the content of the the pin related fields.
+   * @this Blockly.Block
+   */
+  updateFields: function() {
+    Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+        this, 'PIN', 'analogPins');
+  }
+};
+
+Blockly.Blocks['io_pin_pwm'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_PIN_PWM)
+        .appendField(new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.pwmPins), "PIN");
+    this.setOutput(true);
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.setTooltip(Blockly.Msg.ARD_PIN_PWM_TIP);
+    this.setHelpUrl('https://www.arduino.cc/en/Reference/Board');
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.NUMBER;
+  },
+  /**
+   * Updates the content of the the pin related fields.
+   * @this Blockly.Block
+   */
+  updateFields: function() {
+    Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+        this, 'PIN', 'pwmPins');
+  }
+};
+
+Blockly.Blocks['io_digitalwrite_var'] = {
+  /**
+   * Block for creating a 'set pin' to a state.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/DigitalWrite');
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.appendValueInput('STATE')
+        .appendField(Blockly.Msg.ARD_DIGITALWRITE)
+        .appendField(new Blockly.FieldVariable("item"), "PIN")
+        .appendField(Blockly.Msg.ARD_WRITE_TO)
+        .setCheck(Blockly.Types.BOOLEAN.compatibles());
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.ARD_DIGITALWRITEVAR_TIP);
+  }
+};
+
+Blockly.Blocks['io_digitalread_var'] = {
+  /**
+   * Block for creating a 'read pin'.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/DigitalRead');
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_DIGITALREAD)
+        .appendField(new Blockly.FieldVariable("item"), "PIN");
+    this.setOutput(true, Blockly.Types.BOOLEAN.basicType);
+    this.setTooltip(Blockly.Msg.ARD_DIGITALREAD_TIP);
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.BOOLEAN;
+  }
+};
+
+Blockly.Blocks['io_analogwrite_var'] = {
+  /**
+   * Block for creating a 'set pin' to an analogue value.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/AnalogWrite');
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.appendValueInput('NUM')
+        .appendField(Blockly.Msg.ARD_ANALOGWRITE)
+        .appendField(new Blockly.FieldVariable("item"), "PIN")
+        .appendField(Blockly.Msg.ARD_WRITE_TO)
+        .setCheck(Blockly.Types.NUMBER.basicType);
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.ARD_ANALOGWRITE_TIP);
+  },
+  /**
+   * Updates the content of the the pin related fields.
+   * @this Blockly.Block
+   */
+  updateFields: function() {
+    Blockly.Arduino.Boards.refreshBlockFieldDropdown(this, 'PIN', 'pwmPins');
+  },
+  /** @return {!string} The type of input value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.NUMBER;
+  },
+};
+
+Blockly.Blocks['io_analogread_var'] = {
+  /**
+   * Block for reading an analogue input.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/AnalogRead');
+    this.setColour(Blockly.Blocks.io.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_ANALOGREAD)
+        .appendField(new Blockly.FieldVariable("item"), "PIN");
+    this.setOutput(true, Blockly.Types.NUMBER.basicType);
+    this.setTooltip(Blockly.Msg.ARD_ANALOGREAD_TIP);
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function() {
+    return Blockly.Types.NUMBER;
+  },
+  /**
+   * Updates the content of the the pin related fields.
+   * @this Blockly.Block
+   */
+  updateFields: function() {
+    Blockly.Arduino.Boards.refreshBlockFieldDropdown(this, 'PIN', 'analogPins');
+  }
+};
---
+++ blockly/generators/arduino/io_extra.js
@@ -0,0 +1,119 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Code generator for Arduino Digital and Analogue input/output.
+ *     Arduino built in function docs: http://arduino.cc/en/Reference/HomePage
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.IO_EXTRA');
+
+goog.require('Blockly.Arduino');
+goog.require('Blockly.Arduino.IO');
+
+
+/**
+ * Function for assigning a variable a specific pin.
+ * Arduino code: setup { pinMode(X, OUTPUT); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.Arduino['io_pin_dig'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+
+  var code = pin;
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+/**
+ * Function for 'set pin' (X) to a state (Y).
+ * Arduino code: setup { pinMode(X, OUTPUT); }
+ *               loop  { digitalWrite(X, Y); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.Arduino['io_digitalwrite_var'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+  var stateOutput = Blockly.Arduino.valueToCode(
+      block, 'STATE', Blockly.Arduino.ORDER_ATOMIC) || 'LOW'
+  
+  Blockly.Arduino.reservePin(
+      block, pin, Blockly.Arduino.PinTypes.OUTPUT, 'Digital Write');
+  
+  var pinSetupCode = 'pinMode(' + pin + ', OUTPUT);';
+  Blockly.Arduino.addSetup('io_' + pin, pinSetupCode, false);
+  
+  var code = 'digitalWrite(' + pin + ', ' + stateOutput + ');\n'
+  return code;
+};
+
+/**
+ * Function for reading a digital pin (X).
+ * Arduino code: setup { pinMode(X, INPUT); }
+ *               loop  { digitalRead(X)     }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code with order of operation.
+ */
+Blockly.Arduino['io_digitalread_var'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+  Blockly.Arduino.reservePin(
+      block, pin, Blockly.Arduino.PinTypes.INPUT, 'Digital Read');
+
+  var pinSetupCode = 'pinMode(' + pin + ', INPUT);';
+  Blockly.Arduino.addSetup('io_' + pin, pinSetupCode, false);
+
+  var code = 'digitalRead(' + pin + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+/**
+ * Function for setting the state (Y) of an analogue output (X).
+ * Arduino code: setup { pinMode(X, OUTPUT); }
+ *               loop  { analogWrite(X, Y);  }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.Arduino['io_analogwrite_var'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+  var stateOutput = Blockly.Arduino.valueToCode(
+      block, 'NUM', Blockly.Arduino.ORDER_ATOMIC) || '0';
+
+  Blockly.Arduino.reservePin(
+      block, pin, Blockly.Arduino.PinTypes.OUTPUT, 'Analogue Write');
+
+  var pinSetupCode = 'pinMode(' + pin + ', OUTPUT);';
+  Blockly.Arduino.addSetup('io_' + pin, pinSetupCode, false);
+
+  // Warn if the input value is out of range
+  if ((stateOutput < 0) || (stateOutput > 255)) {
+    block.setWarningText('The analogue value set must be between 0 and 255',
+                         'pwm_value');
+  } else {
+    block.setWarningText(null, 'pwm_value');
+  }
+
+  var code = 'analogWrite(' + pin + ', ' + stateOutput + ');\n';
+  return code;
+};
+
+/**
+ * Function for reading an analogue pin value (X).
+ * Arduino code: setup { pinMode(X, INPUT); }
+ *               loop  { analogRead(X)      }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code with order of operation.
+ */
+Blockly.Arduino['io_analogread_var'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+  Blockly.Arduino.reservePin(
+      block, pin, Blockly.Arduino.PinTypes.INPUT, 'Analogue Read');
+
+  var pinSetupCode = 'pinMode(' + pin + ', INPUT);';
+  Blockly.Arduino.addSetup('io_' + pin, pinSetupCode, false);
+
+  var code = 'analogRead(' + pin + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
